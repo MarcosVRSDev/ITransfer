17e2e0ac47de02c2e75d9735a5f1e90ae6019ced
Marcos Rezende <marcosvrs.dev@gmail.com>
2020-04-28 18:15:18 -0300

    Melhorias e adição de funcionalidades gerais

--- src/action.js
+++ src/action.js
@@ -4,20 +4,22 @@ const cmd = require("node-command-line");
 Promise = require("bluebird");
 
 //Dirétorios
-let externalFolder = __dirname + '\\externos\\';
+let externalFolder = __dirname + "\\externos\\";
 let inputEnvio = document.querySelector("#input-envio");
 let inputRetorno = document.querySelector("#input-retorno");
+let defaultWM = "Program Files/";
+let defaultAndroid = "/sdcard/";
 
+//Elementos na DOM
 let inputArquivo = document.querySelector("#input-arquivo");
 let searchEnvio = document.querySelector("#search-envio");
 let searchRetorno = document.querySelector("#search-retorno");
-let modalProgress = document.querySelector('#modal-progress');
-let processType = document.querySelector('#process-type');
+let modalProgress = document.querySelector("#modal-progress");
+let modalSetting = document.querySelector("#modal-settings");
+let processType = document.querySelector("#process-type");
 
 //Trás os diretórios salvos em LocalStorage ao abrir o sistema
 populateUI();
-console.log(__dirname + "\\utils\\logo.svg");
-
 
 searchEnvio.addEventListener("change", (event) => {
   let newPath = path.dirname(searchEnvio.files[0].path);
@@ -29,45 +31,48 @@ searchRetorno.addEventListener("change", (event) => {
   inputRetorno.value = newPath;
 });
 
-
 //Enviar arquivo
 function sendFile() {
-  
- modalProgress.classList.add('show');
-
-  if(processType.value == 1) {
-
+  modalProgress.classList.add("show");
+  //PROCESSO DE ENVIO WINDOWS MOBILE
+  if (processType.value == 1) {
     Promise.coroutine(function* () {
-      var response = yield cmd.run(
-        `${externalFolder}ConsoleApp1.exe -E ${inputEnvio.value}\\${inputArquivo.value} "Program Files/${inputArquivo.value}"`
+      let response = yield cmd.run(
+        `${externalFolder}ConsoleApp1.exe -WM -E "${inputEnvio.value}\\${inputArquivo.value}" "${defaultWM}${inputArquivo.value}"`
       );
       if (response.success) {
-        alert("Arquivo enviado com sucesso!\n" + response.message);
-        modalProgress.classList.remove('show');
         // do something
         // if success get stdout info in message. like response.message
+        alert("Arquivo enviado com sucesso!\n" + response.message);
+        modalProgress.classList.remove("show");
       } else {
-        alert("Não foi possível enviar o arquivo. Motivo: " + response.error);
-        modalProgress.classList.remove('show');
-        // do something
         // if not success get error message and stdErr info as error and stdErr.
         //like response.error and response.stdErr
-    }
-  })();
-
-  /* Mover arquivos entre pastas - Estara disponivel em outra rotina
-    (async () => {
-      await moveFile(
-        `${inputEnvio.value}/${inputArquivo.value}`,
-        `${inputRetorno.value}/${inputArquivo.value}`
+        alert("Não foi possível enviar o arquivo. Motivo: " + response.error);
+        modalProgress.classList.remove("show");
+      }
+    })();
+  } //PROCESSO DE ENVIO DO ANDROID
+  else if (processType.value == 2) {
+    Promise.coroutine(function* () {
+      let response = yield cmd.run(
+        `${externalFolder}ConsoleApp1.exe -A -E "${inputEnvio.value}\\${inputArquivo.value}" "${defaultAndroid}${inputArquivo.value}"`
       );
-      alert("O arquivo foi enviado com sucesso!");
-      localStorage.setItem("inputEnvio", inputEnvio.value);
-      localStorage.setItem("inputRetorno", inputRetorno.value);
-    })();*/
+      if (response.success) {
+        // do something
+        // if success get stdout info in message. like response.message
+        alert("Arquivo enviado com sucesso!\n" + response.message);
+        modalProgress.classList.remove("show");
+      } else {
+        // if not success get error message and stdErr info as error and stdErr.
+        //like response.error and response.stdErr
+        alert("Não foi possível enviar o arquivo. Motivo: " + response.error);
+        modalProgress.classList.remove("show");
+      }
+    })();
   } else {
     alert("Processo de envio ainda não disponível.");
-    modalProgress.classList.remove('show');
+    modalProgress.classList.remove("show");
   }
 
   localStorage.setItem("inputEnvio", inputEnvio.value);
@@ -77,32 +82,75 @@ function sendFile() {
 
 //Receber arquivo
 function getFile() {
-  (async () => {
-    await moveFile(
-      `${inputRetorno.value}/${inputArquivo.value}`,
-      `${inputEnvio.value}/${inputArquivo.value}`
-    );
-    alert("O arquivo foi recebido com sucesso!");
-    localStorage.setItem("inputEnvio", inputEnvio.value);
-    localStorage.setItem("inputRetorno", inputRetorno.value);
-  })();
+  modalProgress.classList.add("show");
+  //PROCESSO DE ENVIO WINDOWS MOBILE
+  if (processType.value == 1) {
+    Promise.coroutine(function* () {
+      let response = yield cmd.run(
+        `${externalFolder}ConsoleApp1.exe -WM -R "${defaultWM}${inputArquivo.value}" "${inputEnvio.value}\\${inputArquivo.value}"`
+      );
+      if (response.success) {
+        // do something
+        // if success get stdout info in message. like response.message
+        alert("Arquivo recebido com sucesso!\n" + response.message);
+        modalProgress.classList.remove("show");
+      } else {
+        // if not success get error message and stdErr info as error and stdErr.
+        //like response.error and response.stdErr
+        alert("Não foi possível receber o arquivo. Motivo: " + response.error);
+        modalProgress.classList.remove("show");
+      }
+    })();
+  } //PROCESSO DE ENVIO DO ANDROID
+  else if (processType.value == 2) {
+    Promise.coroutine(function* () {
+      let response = yield cmd.run(
+        `${externalFolder}ConsoleApp1.exe -A -R "${defaultAndroid}${inputArquivo.value}" "${inputEnvio.value}\\${inputArquivo.value}"`
+      );
+      if (response.success) {
+        // do something
+        // if success get stdout info in message. like response.message
+        alert("Arquivo recebido com sucesso!\n" + response.message);
+        modalProgress.classList.remove("show");
+      } else {
+        // if not success get error message and stdErr info as error and stdErr.
+        //like response.error and response.stdErr
+        alert("Não foi possível receber o arquivo. Motivo: " + response.error);
+        modalProgress.classList.remove("show");
+      }
+    })();
+  } else {
+    alert("Processo de recebimento ainda não disponível.");
+    modalProgress.classList.remove("show");
+  }
+
+  localStorage.setItem("inputEnvio", inputEnvio.value);
+  localStorage.setItem("inputRetorno", inputRetorno.value);
+  localStorage.setItem("progressType", processType.value);
 }
 
 //Função para mudar a informação de diretório de acordo com tipo de processo de envio escolhido (Versão 1.0 o diretório é encapsulado no código)
 function changeDefaultFolder() {
-  if(processType.value == 1) {
-    inputRetorno.value = "Program Files/";
+  if (processType.value == 1) {
+    inputRetorno.value = defaultWM;
     localStorage.setItem("inputRetorno", inputRetorno.value);
     localStorage.setItem("progressType", processType.value);
-
   } else if (processType.value == 2) {
-    inputRetorno.value = '';
+    inputRetorno.value = defaultAndroid;
     localStorage.setItem("inputRetorno", inputRetorno.value);
     localStorage.setItem("progressType", processType.value);
-
   } else {
-    inputRetorno.value = '';
+    inputRetorno.value = "";
   }
+}
+
+//Função para abrir o modal de settings
+function openSetting() {
+  modalSetting.classList.add("show");
+}
+
+function closeSetting() {
+  modalSetting.classList.remove("show");
 }
 
 //Função que trás os diretórios salvos em LocalStorage
